77be5eac3722e04d89574bdffac5bfb31af88771
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2019-07-08 16:43:42 -0500

    Nueva adición

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -4,23 +4,31 @@ const {
 } = require('mongoose')
 
 const restaurantSchema = new Schema({
-    name: String,
-    giro: {
-        type: String require,
-        enum: ['Restaurant', 'Bar', 'Antro', 'Cantina'],
-        default: 'Restaurant'
-    },
-    averagePrice: Number,
-    creatorId: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-    imgPath: String,
-    imgName: String,
-    calification: [type: String]
-}, {
-    timestamps: true,
-    versionKey: false
-})
+        name: String,
+        giro: {
+            type: String require,
+            enum: ['Restaurant', 'Bar', 'Antro', 'Cantina'],
+            default: 'Restaurant'
+        },
+        location: {
+            addres: {
+                type: String,
+                default: 'Point',
+            },
+            coordinates: [Number]
+        },
+        averagePrice: Number,
+        creatorId: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+        imgPath: String,
+        imgName: String,
+        calification: [Number]
 
+    }, {
+        timestamps: true,
+        versionKey: false
+    })
+    //HOLA CRIS
 module.exports = model('User', UserSchema)
