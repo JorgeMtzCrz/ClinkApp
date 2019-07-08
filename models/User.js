447cfa91f9278755dcb2678094bb521560b8c587
sofia <sofleos@gmail.com>
2019-07-08 17:19:42 -0500

    nada importante

--- models/User.js
+++ models/User.js
@@ -1,24 +1,27 @@
-const {
-    model,
-    Schema
-} = require('mongoose')
-const plm = require('passport-local-mongoose')
+const { model, Schema } = require("mongoose");
+const plm = require("passport-local-mongoose");
 
-const userSchema = new Schema({
-    name: String require,
-    email: String require,
+const userSchema = new Schema(
+  {
+    name: String,
+    require,
+    email: String,
+    require,
     role: {
-        type: String require,
-        enum: ['user', 'barAdmin', 'admin'],
-        default: 'user'
+      type: String,
+      require,
+      enum: ["user", "barAdmin", "admin"],
+      default: "user"
     }
-}, {
+  },
+  {
     timestamps: true,
     versionKey: false
-})
+  }
+);
 
 UserSchema.plugin(plm, {
-    usernameField: 'email'
-})
+  usernameField: "email"
+});
 
-module.exports = model('User', UserSchema)
+module.exports = model("User", UserSchema);
