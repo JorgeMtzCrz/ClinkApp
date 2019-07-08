122caaba51bf60a1a4342bcd4752497186fa2246
sofia <sofleos@gmail.com>
2019-07-08 16:23:40 -0500

    index

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const passport = require('../config/passport')
-
-/* GET home page */
-router.get('/', (req, res, next) => {
-    res.render('index');
-});
-
-module.exports = router;
-=======
 const express = require("express");
 const router = express.Router();
 const {} = require();
@@ -26,4 +14,3 @@ router.get("/cocktail-last");
 router.get("/acomp-last");
 
 module.exports = router;
->>>>>>> f2ba5fa2f4098ab8336b850668fa4e925037a608
