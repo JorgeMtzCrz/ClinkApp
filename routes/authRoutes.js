47868bb24833882a4eb3d07cd386c0626b050189
sofia <sofleos@gmail.com>
2019-07-09 10:38:36 -0500

    con los cambios de rutas y passport

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,75 +1,32 @@
-const express = require("express");
-const passport2 = require("passport");
-const router = express.Router();
+const { Router } = require("express");
+const router = Router();
+const passport = require("../middlewares/passport");
 const {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    postSignup,
-    getSignup,
-    getRestaurant,
-    getLogin,
-    postLogin,
-    getProfile,
-    logout
-} = require('../controllers/authControllers')
-const {
-    isLoggedIn,
-    isAuth
-} = require('../middlewares/auth')
-
->>>>>>> d0f2db1cf7cfa59023ec15b51460a038719d4328
-
-  postSignup,
   getSignup,
-  getRestaurant,
+  postSignup,
   getLogin,
-  postLogin,
-  getProfile
+  logout
 } = require("../controllers/authControllers");
+
 router.get("/signup", getSignup);
 router.post("/signup", postSignup);
-router.get("/altaRes", getRestaurant);
 router.get("/login", getLogin);
-router.post("/login", passport.authenticate("local"), postLogin);
-router.get("/logout");
-router.get("/perfil", getProfile);
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    passReqToCallback: true,
+    failureFlash: true
+  })
+);
 
-const passport = require("../config/passport");
-const { postSignup, getSignup } = require("../controllers/authControllers");
-router.get("/signup", getSignup);
-router.post("/signup", postSignup);
-router.get("/login");
-router.post("/login");
-router.get("/logout");
-router.get("/profile");
-<<<<<<< HEAD
-
-    postSignup,
-    getSignup,
-    getRestaurant,
-    getLogin,
-    postLogin,
-    getProfile,
-    logout
-} = require('../controllers/authControllers')
-const {
-    isLoggedIn,
-    isAuth
-} = require('../middlewares/auth')
-
-
-router.get("/signup", getSignup);
-router.post("/signup", postSignup);
-router.get("/login", isLoggedIn, getLogin);
-router.post("/login", passport.authenticate('local'), postLogin);
 router.get("/logout", logout);
-router.get(`/perfil`, isAuth, getProfile);
-router.get("/altares", isAuth, getRestaurant)
-
-=======
->>>>>>> 9893df45cd639d875a8db637e9f145120448f3b1
->>>>>>> b26354db97e075d05ecce57714a51ed941158bc9
->>>>>>> d0f2db1cf7cfa59023ec15b51460a038719d4328
-
-module.exports = router;
+router.get("/slack", passport.authenticate("slack"));
+router.get(
+  "/slack/callback",
+  passport.authenticate("slack", {
+    successRedirect: "/private-page",
+    failureRedirect: "/auth/login"
+  })
+);
