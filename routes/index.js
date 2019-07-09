2eb4589ffcdb0fb781e74668734d8bca7760defb
sofia <sofleos@gmail.com>
2019-07-09 15:31:34 -0500

    ruta de preguntas

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { getPreguntas, getResults } = require("../controllers/indexControllers");
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-// router.get("/fiesta");
+router.get("/preguntas", getPreguntas);
+router.get("/resultados", getResults);
 // router.get("/cocktail");
 // router.get("/acomp");
 // router.get("/fiesta-last");
