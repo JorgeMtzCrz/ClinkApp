9709a97c142b61b3f5cf0316c15823352dbfb7e6
sofia <sofleos@gmail.com>
2019-07-10 10:11:11 -0500

    con el primer query

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -2,11 +2,16 @@ const Restaurant = require("../models/Restaurant");
 const Drink = require("../models/Drink");
 
 exports.getPreguntas = (req, res, next) => {
+  console.log("Este es el body de preguntas" + req.body);
   res.render("preguntas");
 };
 
-exports.getResults = (req, res, next) => {
-  Restaurant.find().then(restaurants => {
+exports.postPreguntas = async (req, res, next) => {
+  const { averagePrice } = req.body;
+  console.log(req.body.averagePrice);
+  const restaurants = await Restaurant.find({
+    $and: [{ averagePrice: { $eq: `${averagePrice}` } }]
+  }).then(restaurants => {
     res.render("resultados", { restaurants });
   });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getPreguntas, getResults } = require("../controllers/indexControllers");
+const {
+  getPreguntas,
+  postPreguntas
+} = require("../controllers/indexControllers");
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
 router.get("/preguntas", getPreguntas);
-router.get("/resultados", getResults);
+router.post("/preguntas", postPreguntas);
 // router.get("/cocktail");
 // router.get("/acomp");
 // router.get("/fiesta-last");
