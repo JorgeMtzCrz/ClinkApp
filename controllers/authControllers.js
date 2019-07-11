091ec7628be43ef415e05217d88137f397ea2fba
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2019-07-11 11:44:40 -0500

    CRUD COmpleto sin bugs

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -171,15 +171,24 @@ exports.getEditRest = async(req, res, next) => {
         restaurant,
         drinks
     })
+    console.log()
 }
 
 exports.postEditRest = async(req, res, next) => {
+    const {
+        name,
+        giro,
+        averagePrice,
+        drinks
+    } = req.body
     const restaurant = await Restaurant.findByIdAndUpdate(req.params.restaurantID, {
-        $set: {
-            ...req.body
+        name,
+        giro,
+        averagePrice,
+        $push: {
+            drinks: drinks
         }
-    }, {
-        new: true
+
     }).populate('drinks')
     res.redirect(`/auth/perfil`)
 }
