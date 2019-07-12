a6c356669baaf3da2c78220d5056af5e828dea29
sofia <sofleos@gmail.com>
2019-07-12 02:22:04 -0500

    mas responsive

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,32 +1,105 @@
 const Restaurant = require("../models/Restaurant");
+const Drink = require("../models/Drink");
 
-exports.getPreguntas = (req, res, next) => {
-  res.render("preguntas");
+// exports.getPreguntas = async (req, res, next) => {
+//   const drinks = await Drink.find();
+//   const restaurant = await Restaurant.find().populate("drinks");
+//   console.log(restaurant);
+//   res.render("preguntas", {
+//     restaurant,
+//     drinks
+//   });
+//   // res.render("preguntas");
+// };
+
+// exports.postPreguntas = async (req, res, next) => {
+//   const { averagePrice, giro, alcohol, typeDrink } = req.body;
+
+//   const restaurant = await Restaurant.aggregate([
+//     {
+//       $match: {
+//         giro,
+//         averagePrice
+//       }
+//     },
+//     {
+//       $lookup: {
+//         from: "drinks",
+//         let: {
+//           alcool: "alcohol",
+//           typooo: "typeDrink"
+//         },
+//         pipeline: [
+//           {
+//             $match: {
+//               $expr: {
+//                 $and: [
+//                   {
+//                     $eq: [alcohol, "$$alcool"]
+//                   },
+//                   {
+//                     $eq: [typeDrink, "$$typooo"]
+//                   }
+//                 ]
+//               }
+//             }
+//           }
+//         ],
+//         as: "bebidas"
+//       }
+//     }
+//   ]);
+
+//   console.log(restaurant);
+//   res.render("resultados", {
+//     restaurant
+//   });
+exports.getPreguntas = async (req, res, next) => {
+  const drinks = await Drink.find();
+  const restaurant = await Restaurant.find().populate("drinks");
+
+  res.render("preguntas", {
+    restaurant,
+    drinks
+  });
+  // res.render("preguntas");
 };
 
-exports.postPreguntas = (req, res, next) => {
+exports.postPreguntas = async (req, res, next) => {
+  let id;
   const { averagePrice, giro, alcohol, typeDrink } = req.body;
-  console.log(req.body);
-  Restaurant.find({
-    $and: [
-      { averagePrice: { $eq: `${averagePrice}` } },
-      { giro: { $eq: `${giro}` } }
-    ]
-  })
-    .populate({
-      path: "drinks",
-      match: {
-        $and: [
-          { alcohol: { $eq: `${alcohol}` } },
-          { typeDrink: { $eq: `${typeDrink}` } }
-        ]
+
+  const restaurants = await Restaurant.aggregate([
+    {
+      $lookup: {
+        from: "drinks",
+        localField: "drinks",
+        foreignField: "_id",
+        as: "drinks"
       }
-    })
-    .then(restaurants => {
-      console.log(restaurants);
-      res.render("resultados", { restaurants });
-    })
-    .catch(err => next(err));
+    },
+    {
+      $match: {
+        giro,
+        "drinks.0": {
+          $exists: true
+        },
+        "drinks.alcohol": alcohol,
+        "drinks.typeDrink": typeDrink,
+        averagePrice: averagePrice
+      }
+    }
+  ]);
+  for (i = 0; i <= restaurants.length - 1; i++) {
+    console.log("Aqui van las bebidas<<<<<", restaurants[i]._id[i]);
+    const id2 = restaurants[i]._id[i];
+    let id = id2;
+  }
+
+  res.render(`resultados`, {
+    restaurants,
+    id
+  });
 };
 exports.getHome = (req, res, next) => {
   res.redirect("/preguntas");
@@ -36,8 +109,11 @@ exports.getAbout = (req, res, next) => {
   res.render("about");
 };
 
-exports.getDrinkCard = (req, res, next) => {
-  res.render("partials/drinkCard");
+exports.getDrinkCard = async (req, res, next) => {
+  const restaurant = await Restaurant.findById(req.params.id).populate(
+    "drinks"
+  );
+  res.render("detalles", restaurant);
 };
 
 exports.getRestCard = (req, res, next) => {
